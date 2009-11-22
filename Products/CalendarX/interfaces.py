011dbc1eace4c2976e157bae85449b8c8f31bd54
glenfant <glenfant@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-22 11:16:02 +0000

    Exported interfaces

---
+++ Products/CalendarX/interfaces.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# $Id$
+"""CalendarX exposed interfaces"""
+
+from zope.interface import Interface
+
+class ICalendarX(Interface):
+    """Marker interface for CalendarX"""
+    pass
