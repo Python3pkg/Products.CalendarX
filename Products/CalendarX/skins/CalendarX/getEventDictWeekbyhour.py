b05a198ba6e444c5781896a8b6e79250bd7f39b7
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-10-05 13:17:04 +0300

    getEventDictWeekbyhour fixes

--- Products/CalendarX/skins/CalendarX/getEventDictWeekbyhour.py
+++ Products/CalendarX/skins/CalendarX/getEventDictWeekbyhour.py
@@ -4,15 +4,15 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=event, type='C', periodID=None, cDate
+##parameters=event=None, type='C', periodID=None, cDate=None
 ##title=Get dictionary of Event info for weekbyhour view
 ##
 """
-returns a dictionary of useful objects for Events for the Weekbyhour view  
+returns a dictionary of useful objects for Events for the Weekbyhour view
 
 modified for CalendarX-0.6.4 mod Later events to highlight proper date (matches event start date).
 Released under the GPL (see LICENSE.txt)
-event types: 
+event types:
    'C' for continuing event (above main calendar view)
    'E' for normal event (in main calendar view)
    'L' for later event (below main calendar view)
@@ -43,7 +43,7 @@ if useHalfHours:
 else:
     periodsFactor = 1
 periodsInDay = (weekviewendhour - weekviewstarthour)*periodsFactor
-periodsInView = 7*periodsInDay -1 
+periodsInView = 7*periodsInDay -1
 
 #display parameters common to ALL event types
 eventurl =  str(event.getURL())+'/view'
@@ -68,11 +68,11 @@ if type == 'C':    #FOR CONTINUING EVENTS
     daysTilWeekEnd = test(daysTilWeekEnd > 6, 6, daysTilWeekEnd)
     jsStart  = weekviewstarthour*periodsFactor + 1 + (6-daysTilWeekEnd)*periodsInDay
     #0.6.4 change to highlight cEvents cell if an early event (not for actual continuing events)
-    isEarly = earlyHour <= eventend.hour() < weekviewstarthour 
-    jsStart = test(isEarly,weekviewstarthour*periodsFactor,jsStart) 
+    isEarly = earlyHour <= eventend.hour() < weekviewstarthour
+    jsStart = test(isEarly,weekviewstarthour*periodsFactor,jsStart)
     jsEndIfAllWeek = jsStart + periodsInView
   #datetime at start of viewday that event shows up on (at jsStart)
-    dtDayStart = startDate + 6 - daysTilWeekEnd 
+    dtDayStart = startDate + 6 - daysTilWeekEnd
     periodsToShow = context.getNumOfPeriods(dtDayStart, eventend, weekviewstarthour, weekviewendhour)
     jsEnd = jsStart + periodsToShow
   #then test() for ending ON THE HOUR or HALFHOUR, so events don't spill over into the next period upon rollover.
@@ -80,7 +80,7 @@ if type == 'C':    #FOR CONTINUING EVENTS
     eventEndIsInView = weekviewstarthour <= eventend.hour() < weekviewendhour
     if useHalfHours:
         jsEndIfNotAllWeek = test(eventend.minute() in [0,30] or not eventEndIsInView, jsEnd-1, jsEnd)
-    else:   
+    else:
         jsEndIfNotAllWeek = test(eventend.minute() in [0] or not eventEndIsInView, jsEnd-1, jsEnd)
     jsEnd = test(jsEndIfNotAllWeek < jsEndIfAllWeek, jsEndIfNotAllWeek, jsEndIfAllWeek)
   #test() to make sure End is not prior to Start
@@ -99,7 +99,7 @@ if type == 'E':    #FOR EVENTS IN THE REGULAR WEEKBYHOUR TABLE
   #the following test() is for events ending ON THE PERIOD, so they don't spill over into the next period upon rollover.
     if useHalfHours:
         jsEndIfNotAllWeek = test(eventend.minute() in [0,30], jsEnd-1, jsEnd)
-    else:   
+    else:
         jsEndIfNotAllWeek = test(eventend.minute() in [0], jsEnd-1, jsEnd)
     jsEnd = test(jsEndIfNotAllWeek < jsEndIfAllWeek, jsEndIfNotAllWeek, jsEndIfAllWeek)
   #test() to make sure End is not prior to Start
@@ -107,19 +107,19 @@ if type == 'E':    #FOR EVENTS IN THE REGULAR WEEKBYHOUR TABLE
   #generate the eventstring
     sameday = (eday == sday) and (emonth == smonth) and (eyear == syear)
     datestring = (sameday and '%s - %s'%(stime, etime)) or '%s %s - %s %s'%(smonth, sday, emonth, eday)
-    eventstring = '<strong>'+eventtitle+'</strong><br/> '+datestring 
+    eventstring = '<strong>'+eventtitle+'</strong><br/> '+datestring
 
 
 
 #calculate jsStart and jsEnd: integer range of cells in view for hightlighting
 #only highlight ONE Day, the day where the event starts (albeit late)
-if type == 'L':    #FOR LATER EVENTS 
+if type == 'L':    #FOR LATER EVENTS
     firstEveOfWeek = endDate - 6
     evesTilEventStart = context.getNumOfDays(firstEveOfWeek, eventstart) + 1
     jsStart = weekviewstarthour*periodsFactor + 1 + periodsInView + evesTilEventStart
   #test for wee hours events, bring them back to the previous day for highlighting
   #0.6.4 change to highlight previous day cell if a late late event (wee hours) AND not first day of the week
-    isLateLate = 0 <= eventstart.hour() < earlyHour 
+    isLateLate = 0 <= eventstart.hour() < earlyHour
     jsStart = test(isLateLate and evesTilEventStart > 1, jsStart-1, jsStart)
     jsEnd = jsStart
   #generate the eventstring
@@ -131,11 +131,11 @@ if type == 'L':    #FOR LATER EVENTS
 
 #marshall all these into a dictionary
 ewbhdict = {
-         'eventurl':eventurl, 
-         'eventstart':eventstart, 
-         'eventend':eventend, 
-         'jsStart':jsStart, 
-         'jsEnd':jsEnd, 
+         'eventurl':eventurl,
+         'eventstart':eventstart,
+         'eventend':eventend,
+         'jsStart':jsStart,
+         'jsEnd':jsEnd,
          'eventurl':eventurl,
          'eventState':eventState,
          'eventtitle':eventtitle,
@@ -148,7 +148,7 @@ ewbhdict = {
          'emonth':emonth,
          'eday':eday,
          'etime':etime,
-         'eventstring':eventstring, 
+         'eventstring':eventstring,
         }
 
 return ewbhdict
